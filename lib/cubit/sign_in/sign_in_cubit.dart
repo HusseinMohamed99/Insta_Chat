b911378a4bcc2f8a0d31d5c22dc086cac5c3a5d5
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-10-31 15:14:08 +0200

    Add phone authentication and user creation to sign in cubit.

--- lib/cubit/sign_in/sign_in_cubit.dart
+++ lib/cubit/sign_in/sign_in_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -31,5 +32,76 @@ class SignInCubit extends Cubit<SignInState> {
         isPassword ? Icons.visibility_outlined : Icons.visibility_off_outlined;
 
     emit(ChangePasswordState());
+  }
+
+  late String verificationId;
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+  Future<void> submitPhoneNumber(String phoneNumber) async {
+    emit(LoadingState());
+    await FirebaseAuth.instance.verifyPhoneNumber(
+      phoneNumber: '+2$phoneNumber',
+      timeout: const Duration(seconds: 60),
+      verificationCompleted: verificationCompleted,
+      verificationFailed: verificationFailed,
+      codeSent: codeSent,
+      codeAutoRetrievalTimeout: codeAutoRetrievalTimeout,
+    );
+  }
+
+  void verificationCompleted(PhoneAuthCredential credential) async {
+    await signIn(credential);
+  }
+
+  void verificationFailed(FirebaseAuthException error) {
+    emit(PhoneAuthErrorState(messageError: error.toString()));
+  }
+
+  void codeSent(String verificationId, int? resendToken) {
+    this.verificationId = verificationId;
+    emit(PhoneNumberSubmittedState());
+  }
+
+  void codeAutoRetrievalTimeout(String verificationId) {}
+
+  Future<void> submitOTP(String otpCode) async {
+    PhoneAuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: verificationId, smsCode: otpCode);
+    await signIn(credential);
+  }
+
+  Future<void> signIn(PhoneAuthCredential credential) async {
+    try {
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      emit(PhoneOTPVerifiedState());
+    } catch (error) {
+      emit(PhoneAuthErrorState(messageError: error.toString()));
+    }
+  }
+
+  Future<void> addUser({
+    required String name,
+    required String email,
+    required String uid,
+    required String phone,
+    String? image,
+    String? bio,
+    bool? isEmailVerified,
+  }) async {
+    emit(LoginLoadingState());
+
+    return await users.add({
+      name: name,
+      email: email,
+      phone: phone,
+      uid: uid,
+      image:
+          'https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=740&t=st=1698676472~exp=1698677072~hmac=cc82479ff1f2ca3e7720bfb52c550620688b05d13f93a4f5167ae88ca619eab9',
+      bio: 'Write a bio...',
+      isEmailVerified: false,
+    }).then((value) {
+      emit(LoginSuccessState());
+    }).catchError((error) {
+      emit(LoginErrorState(messageError: error));
+    });
   }
 }
--- lib/cubit/sign_in/sign_in_state.dart
+++ lib/cubit/sign_in/sign_in_state.dart
@@ -16,3 +16,25 @@ class SignInErrorState extends SignInState {
 }
 
 class ChangePasswordState extends SignInState {}
+
+class LoadingState extends SignInState {}
+
+class PhoneAuthErrorState extends SignInState {
+  final String messageError;
+
+  PhoneAuthErrorState({required this.messageError});
+}
+
+class PhoneNumberSubmittedState extends SignInState {}
+
+class PhoneOTPVerifiedState extends SignInState {}
+
+class LoginLoadingState extends SignInState {}
+
+class LoginSuccessState extends SignInState {}
+
+class LoginErrorState extends SignInState {
+  final String messageError;
+
+  LoginErrorState({required this.messageError});
+}
