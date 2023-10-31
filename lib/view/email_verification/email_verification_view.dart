fcbafba0f2ac60bdf21ef0757a3aadf992ac4ea2
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-10-31 15:04:48 +0200

    Update email verification screen UI and functionality.

--- lib/view/email_verification/email_verification_view.dart
+++ lib/view/email_verification/email_verification_view.dart
@@ -5,12 +5,12 @@ import 'package:insta_chat/cubit/email_verification/email_verification_cubit.dar
 import 'package:insta_chat/cubit/main/main_cubit.dart';
 import 'package:insta_chat/image_assets.dart';
 import 'package:insta_chat/shared/components/buttons.dart';
-import 'package:insta_chat/shared/components/indicator.dart';
 import 'package:insta_chat/shared/components/navigator.dart';
 import 'package:insta_chat/shared/components/show_toast.dart';
 import 'package:insta_chat/utils/color_manager.dart';
 import 'package:insta_chat/utils/value_manager.dart';
 import 'package:insta_chat/view/onBoard/on_board_view.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class EmailVerificationScreen extends StatelessWidget {
   const EmailVerificationScreen({Key? key}) : super(key: key);
@@ -20,15 +20,22 @@ class EmailVerificationScreen extends StatelessWidget {
     TextTheme textTheme = Theme.of(context).textTheme;
     double screenHeight = MediaQuery.sizeOf(context).height;
     double screenWidth = MediaQuery.sizeOf(context).width;
+    EmailVerificationCubit cubit = EmailVerificationCubit.get(context);
+
     return BlocConsumer<EmailVerificationCubit, EmailVerificationState>(
       listener: (context, state) {
-        if (state is ReloadSuccessState) {
+        if (cubit.isEmailVerified) {
           showToast(
-            text: 'createAccountSuccessfully',
+            text: 'create Account Successfully',
             state: ToastStates.success,
           );
           MainCubit.get(context).getUserData();
           MainCubit.get(context).getAllUsers();
+        } else {
+          showToast(
+            text: 'Please Verify Your Email',
+            state: ToastStates.error,
+          );
         }
         if (state is ReloadErrorState) {
           showToast(
@@ -38,128 +45,141 @@ class EmailVerificationScreen extends StatelessWidget {
         }
       },
       builder: (context, state) {
-        EmailVerificationCubit cubit = EmailVerificationCubit.get(context);
-
         return AnnotatedRegion<SystemUiOverlayStyle>(
           value: const SystemUiOverlayStyle(
             statusBarColor: Colors.transparent,
           ),
-          child: Scaffold(
-            body: SizedBox(
-              height: screenHeight,
-              width: screenWidth,
-              child: Padding(
-                padding: const EdgeInsets.all(AppPadding.p20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(height: screenHeight * .1),
-                    CircleAvatar(
-                      radius: 83,
-                      backgroundColor: ColorManager.error,
-                      child: const CircleAvatar(
-                        radius: 80,
-                        backgroundImage: AssetImage(Assets.imagesEmail),
+          child: ModalProgressHUD(
+            inAsyncCall: state is SendVerificationLoadingState,
+            child: Scaffold(
+              body: SizedBox(
+                height: screenHeight,
+                width: screenWidth,
+                child: Padding(
+                  padding: const EdgeInsets.all(AppPadding.p20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: screenHeight * .1),
+                      CircleAvatar(
+                        radius: 83,
+                        backgroundColor: ColorManager.error,
+                        child: const CircleAvatar(
+                          radius: 80,
+                          backgroundImage: AssetImage(Assets.imagesEmail),
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 15),
-                    Text(
-                      'emailConfirmation',
-                      style: textTheme.headlineMedium,
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      "We're happy you signed up for Socialite. To start exploring the Socialite,Please confirm your\nE-mail Address.",
-                      style: textTheme.titleMedium,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 45),
-                    state is SendVerificationLoadingState
-                        ? const AdaptiveIndicator()
-                        : cubit.isEmailSent
-                            ? Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    width: 250,
-                                    height: 40,
+                      const SizedBox(height: AppSize.s16),
+                      Text(
+                        'Email Confirmation',
+                        style: textTheme.headlineMedium,
+                      ),
+                      const SizedBox(height: AppSize.s10),
+                      Text(
+                        "We're happy you signed up for Socialite. To start exploring the Socialite,Please confirm your\nE-mail Address.",
+                        style: textTheme.titleMedium,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: AppSize.s40),
+                      cubit.isEmailSent
+                          ? Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: AppSize.s250,
+                                  height: AppSize.s40,
+                                  decoration: BoxDecoration(
                                     color: ColorManager.success,
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Icon(
-                                          Icons.check_circle_outline,
-                                          color: ColorManager.black,
-                                        ),
-                                        Text(
-                                          'emailVerification',
-                                          style: textTheme.headlineSmall,
-                                        ),
-                                      ],
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.check_circle_outline,
+                                        color: ColorManager.black,
+                                      ),
+                                      Text(
+                                        'Email Verification',
+                                        style: textTheme.headlineSmall,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(height: AppSize.s16),
+                                TextButton(
+                                  style: TextButton.styleFrom(
+                                    backgroundColor: ColorManager.white,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
                                     ),
                                   ),
-                                  const SizedBox(width: 10),
-                                  TextButton(
-                                    style: TextButton.styleFrom(
-                                      backgroundColor: ColorManager.white,
-                                    ),
-                                    onPressed: () {
-                                      cubit.sendEmailVerification();
-                                    },
-                                    child: Text(
-                                      'sendAgain',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headlineSmall,
-                                    ),
-                                  ),
-                                ],
-                              )
-                            : Container(
-                                color: ColorManager.primaryColor,
-                                child: defaultTextButton(
-                                  context: context,
-                                  function: () {
+                                  onPressed: () {
                                     cubit.sendEmailVerification();
                                   },
-                                  text: 'sendEmail',
+                                  child: Text(
+                                    'Send Again',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineSmall,
+                                  ),
                                 ),
+                              ],
+                            )
+                          : Container(
+                              decoration: BoxDecoration(
+                                color: ColorManager.white,
+                                borderRadius: BorderRadius.circular(10),
                               ),
-                    const SizedBox(height: 15),
-                    cubit.isEmailSent
-                        ? Container(
-                            color: ColorManager.success,
-                            child: defaultTextButton(
-                              context: context,
-                              function: () {
-                                cubit.reloadUser().then(
-                                  (value) {
-                                    if (cubit.isEmailVerified) {
-                                      MainCubit.get(context)
-                                        ..getUserData()
-                                        ..getAllUsers();
+                              child: defaultTextButton(
+                                context: context,
+                                function: () {
+                                  cubit.sendEmailVerification();
+                                },
+                                text: 'Send Email',
+                              ),
+                            ),
+                      const SizedBox(height: AppSize.s16),
+                      cubit.isEmailSent
+                          ? Container(
+                              decoration: BoxDecoration(
+                                color: ColorManager.success,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: defaultTextButton(
+                                context: context,
+                                function: () {
+                                  cubit.reloadUser().then(
+                                    (value) {
+                                      if (cubit.isEmailVerified) {
+                                        MainCubit.get(context)
+                                          ..getUserData()
+                                          ..getAllUsers();
 
-                                      navigateAndFinish(
-                                        context,
-                                        const OnBoardScreen(),
-                                      );
-                                    } else {}
-                                  },
-                                );
-                              },
-                              text: 'verified',
+                                        navigateAndFinish(
+                                          context,
+                                          const OnBoardScreen(),
+                                        );
+                                      } else {}
+                                    },
+                                  );
+                                },
+                                text: 'Verified',
+                              ),
+                            )
+                          : Container(
+                              padding: const EdgeInsets.all(AppPadding.p12),
+                              decoration: BoxDecoration(
+                                color: ColorManager.white,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Text(
+                                'Verified',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
                             ),
-                          )
-                        : Container(
-                            color: ColorManager.white,
-                            child: defaultTextButton(
-                              context: context,
-                              function: () {},
-                              text: 'verified',
-                            ),
-                          ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
